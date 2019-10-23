a43c609b95fd64a10dc8e86ff8fad7918e58043d
SirWill <dev_will@hotmail.com>
2019-10-23 09:11:20 -0300

    Criando página home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { MapaService } from '../services/mapa.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  @ViewChild('map', { read: ElementRef, static: true }) mapElement: ElementRef;
+  @ViewChild('pacInput', { read: ElementRef, static: true }) pacInputElement: ElementRef;
+  map: any;
 
-  constructor() {}
+  constructor(private mapaService: MapaService) {}
 
+  ionViewDidEnter() {
+    this.loadMap();
+  }
+
+  async loadMap() {
+    await this.mapaService.loadMap(this.mapElement).then(map => (this.map = map));
+    this.mapaService.showItens(this.map);
+    this.mapaService.addSearchBox(this.map, this.pacInputElement);
+    this.mapaService.markerClick(this.map);
+  }
 }
