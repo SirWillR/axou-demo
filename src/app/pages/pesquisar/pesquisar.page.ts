ce21f890e760fbb8288e1c7c173922bc9ab35fff
SirWill <dev_will@hotmail.com>
2019-10-24 11:06:39 -0300

    Criando pagina pesquisar

---
+++ src/app/pages/pesquisar/pesquisar.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pesquisar',
+  templateUrl: './pesquisar.page.html',
+  styleUrls: ['./pesquisar.page.scss']
+})
+export class PesquisarPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
