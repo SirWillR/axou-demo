fe8eefe7c7e0cda2f3844b6ddfffb21117725f2c
SirWill <dev_will@hotmail.com>
2019-10-26 01:17:37 -0300

    Enviando para produção

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDsPZ1_Xe_Z1vMf817hDF7DMXeFlPjuWIU',
+    authDomain: 'axou-demo.firebaseapp.com',
+    databaseURL: 'https://axou-demo.firebaseio.com',
+    projectId: 'axou-demo',
+    storageBucket: 'axou-demo.appspot.com',
+    messagingSenderId: '276885526220',
+    appId: '1:276885526220:web:72e21d1b4e9c72387c1469',
+    measurementId: 'G-TVJB8J7GP8'
+  }
 };
